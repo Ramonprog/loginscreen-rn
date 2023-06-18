03da4cc200c5acb640e68967b7991517917a6e34
Ramonprog <programador.ramon@gmail.com>
2023-06-18 17:46:49 -0300

    estrutura do projeto

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>teste</Text>
+      <Image source={require("./assets/login.jpg")} />
+      <Text>Sistema de Login</Text>
+      <Text>Bem vindo(a)! Digite seus dados abaixo.</Text>
+
+      <View>
+        <Text> Email: </Text>
+        <TextInput placeholder="Digite seu Email" />
+      </View>
+
+      <View>
+        <Text>Senha: </Text>
+        <TextInput placeholder="******" secureTextEntry />
+      </View>
+
+      <View>
+        <TouchableOpacity>
+          <Text>Esqueci minha senha</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text>Não tem uma conta?</Text>
+        <TouchableOpacity>
+          <Text>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text>Criado por @ramon</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +57,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: "100px",
+    height: "100px",
   },
 });
